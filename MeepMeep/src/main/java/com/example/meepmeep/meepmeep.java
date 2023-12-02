cf28421c2d159dd3bd15fa2d553f27889daf6c3c
916-Sala-Darius <saladarius72@gmail.com>
2023-12-02 22:35:18 +0200

    2

--- MeepMeep/src/main/java/com/example/meepmeep/meepmeep.java
+++ MeepMeep/src/main/java/com/example/meepmeep/meepmeep.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -11,17 +12,33 @@ public class meepmeep {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(75, 75, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(23, -61.5, Math.toRadians(90)))
+                                .lineTo(new Vector2d(10, -34))
+                                .strafeRight(5)
+                                .back(5)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                .strafeRight(2)
+                                .back(25)
+                                .back(5)
+                                .forward(85)
+                                .strafeRight(1.5)
+                                .strafeLeft(1.5)
+                                .back(85)
+                                .forward(85)
+                                .strafeRight(1.5)
+                                .strafeLeft(1.5)
+                                .back(85)
+                                .forward(85)
+                                .strafeRight(1.5)
+                                .strafeLeft(1.5)
+                                .back(85)
+                                .forward(85)
+                                .strafeRight(13.5)
+                                .strafeLeft(13.5)
+                                .back(8)
+
                                 .build()
                 );
 
---
+++ TeamCode/src/main/java/Traiectorie1.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+public class Traiectorie1 {
+}
